4353c81424215d1bf846b78b38fe8cc4eda03e27
Doug Kintop <dkintop@hotmail.com>
2020-01-24 15:05:39 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,32 +1,5 @@
-// import { Component } from "react";
-
-// // Code EyesOnMe Component Here
-// export default class EyesOnMe extends React.component {
-//   constructor() {
-//     super();
-//   }
-
-//   render() {
-//     <button
-//       onFocus={console.log("Good!")}
-//       onBlur={console.log("Hey! Eyes on me!")}
-//     />;
-//   }
-// }
 import React from "react";
 
-class EyesOnMe extends React.Component {
-  onFocus = () => console.log("Good!");
-
-  onBlur = () => console.log("Hey! Eyes on me!");
-
-  render() {
-    return (
-      <button onFocus={this.onFocus} onBlur={this.onBlur}>
-        Eyes on me, please!
-      </button>
-    );
-  }
-}
+class EyesOnMe extends React.Component {}
 
 export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,16 +1,3 @@
-// Code Keypad Component Here
-// import { Component } from "react";
 
-// export default class Keypad extends React.component {
-//   constructor() {
-//     super();
-//   }
 
-//   render() {
-//     return (
-//       <div>
-//         <input type="password" onKeyUp={console.log("entering password...")} />
-//       </div>
-//     );
-//   }
-// }
+
