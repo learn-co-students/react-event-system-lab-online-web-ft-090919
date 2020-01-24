ea9c0d6cb7ef43fe131b92557942fc5580c95cf6
Doug Kintop <dkintop@hotmail.com>
2020-01-24 14:49:55 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,8 @@
+import { Component } from "react";
+
 // Code EyesOnMe Component Here
+export default class EyesOnMe extends Component {
+  constructor() {
+    super();
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import { Component } from "react";
+
+export default class Keypad extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <input type="password" onKeyUp={console.log("entering password...")} />
+    );
+  }
+}
